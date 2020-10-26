e91e829abb499b8dc3cd722f981db563f1264329
as-you-say <dgf5820@daum.net>
2020-10-26 19:38:18 +0900

    Updates

---
+++ precache-manifest.b51515abc9b22ed0c7c21e7708dd4010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbd856ccd05d701cc32922f255e6ed2",
+    "url": "/Landing-SSOBoard/index.html"
+  },
+  {
+    "revision": "f6ee36e46258aaccd876",
+    "url": "/Landing-SSOBoard/static/css/main.bca7b34d.chunk.css"
+  },
+  {
+    "revision": "08c2d536bf7e7ea3194c",
+    "url": "/Landing-SSOBoard/static/js/2.5499fc3a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Landing-SSOBoard/static/js/2.5499fc3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ee36e46258aaccd876",
+    "url": "/Landing-SSOBoard/static/js/main.7205364e.chunk.js"
+  },
+  {
+    "revision": "a838f8fdab684a7ae26e",
+    "url": "/Landing-SSOBoard/static/js/runtime-main.f42b7266.js"
+  }
+]);
